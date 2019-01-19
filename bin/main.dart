e5dfe58d5fcdd4591e7dbd9b3e62c3d9bd010508
Lukas Herman <lherman.cs@gmail.com>
2019-01-19 15:41:02 -0500

    Redesigned program flow to be more async

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,12 @@
 import 'dart:io';
+import 'dart:async';
 import 'package:vscreen_client_core/vscreen_client_core.dart';
+import 'dart:convert';
+
+Stream readLine() =>
+    stdin.transform(Utf8Decoder()).transform(new LineSplitter());
+
+void processCommand(String line) {}
 
 main(List<String> arguments) async {
   Map<String, dynamic> config = {
@@ -8,38 +15,53 @@ main(List<String> arguments) async {
     "password": "poop"
   };
 
-  if (arguments.length == 0) {
-    print("[error] usage: <command> args...");
-    exit(-1);
-  }
+  var vscreenBloc = VScreenBloc();
+  vscreenBloc.connection
+      .dispatch(Connect(url: config["url"], port: config["port"]));
 
-  var vscreen = VScreen(config["url"], config["port"]);
-  await vscreen.auth(config["password"]);
+  ProcessSignal.sigint.watch().listen((ProcessSignal signal) {
+    print("cleaning up");
+    vscreenBloc.dispose();
+    exit(0);
+  });
 
-  var op = arguments[0];
+  var playerBloc = vscreenBloc.player;
 
-  switch (op) {
-    case "play":
-      await vscreen.play();
-      break;
-    case "pause":
-      await vscreen.pause();
-      break;
-    case "stop":
-      await vscreen.stop();
-      break;
-    case "next":
-      await vscreen.next();
-      break;
-    case "add":
-      var url = arguments[1];
-      await vscreen.add(url);
-      break;
-    case "seek":
-      double position = double.tryParse(arguments[1]) ?? 0.0;
-      await vscreen.seek(position);
-      break;
-  }
+  playerBloc.state.listen((playerState) {
+    print("Title        : ${playerState.title}");
+    print("Thumbnail URL: ${playerState.thumbnailURL}");
+    print("Position     : ${playerState.position}");
+    print("Playing      : ${playerState.isPlaying}");
+  });
 
-  await vscreen.close();
+  readLine().listen((line) {
+    var splitted = line.split(" ");
+    var op = splitted[0];
+
+    switch (op) {
+      case "play":
+        playerBloc.dispatch(Play());
+        break;
+      case "pause":
+        playerBloc.dispatch(Pause());
+        break;
+      case "stop":
+        playerBloc.dispatch(Stop());
+        break;
+      case "next":
+        playerBloc.dispatch(Next());
+        break;
+      case "add":
+        var url = splitted[1];
+        playerBloc.dispatch(Add(url));
+        break;
+      case "seek":
+        double position = double.tryParse(splitted[1]) ?? 0.0;
+        playerBloc.dispatch(Seek(position));
+        break;
+    }
+  }).onDone(() {
+    print("cleaning up...");
+    vscreenBloc.dispose();
+  });
 }
